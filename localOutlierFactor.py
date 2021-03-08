f0aeae201d6450ca46403bc5655fa98314a9eb7a
SiddharthRajaraman <71802560+SiddharthRajaraman@users.noreply.github.com>
2021-03-08 17:57:13 -0500

    Update localOutlierFactor.py

--- localOutlierFactor.py
+++ localOutlierFactor.py
@@ -16,6 +16,21 @@ df = df.dropna(subset = ['TEMP'])
 df = df['pH']
 df = df.to_frame()
 
+dataList = df['pH'].values.tolist()
+
+#pH
+dataList.insert(200, 10.2)
+dataList.insert(100, 6.4)
+dataList.insert(500, 9.7)
+dataList.insert(180, 6.6)
+dataList.insert(20, 9)
+dataList.insert(300, 8.6)
+dataList.insert(500, 6.7)
+dataList.insert(310, 10)
+dataList.insert(112, 10)
+dataList.insert(70, 6.8)
+df = pd.DataFrame(dataList, columns = ['pH'])
+
 
 
 model1 = LocalOutlierFactor(n_neighbors = 23, metric = "minkowski", contamination = 0.03)
